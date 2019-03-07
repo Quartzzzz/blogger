d2419b343e56badbab842a3d7830e4afca0888e7
Quentin Mazet <quentinmazet@laposte.net>
2019-03-07 17:24:42 +0100

    Cool article website

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,39 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
 	def index
 		@articles = Article.all
 	end
 	def show
 		@article = Article.find(params[:id])		
+	end
+	def new
+		@article = Article.new		
+	end	
+	def create
+		@article = Article.new(article_params)
+		@article.save
+
+		flash.notice = "Article '#{@article.title}' created. It is already fake news if it disagrees with what Donald Trump believes in"
+
+		redirect_to article_path(@article)
+	end
+	def edit
+		@article = Article.find(params[:id])
+	end
+	def update
+		@article = Article.find(params[:id])
+  	@article.update(article_params)
+
+		flash.notice = "Article '#{@article.title}' updated, I am proud of you son."
+
+  	redirect_to article_path(@article)
+	end
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+	
+		flash.notice = "Article '#{@article.title}' deleted, so long old friend..."	
+
+		redirect_to articles_path
 	end
 end
